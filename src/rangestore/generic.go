3dfccff8884a70ced60225f30a8b7bc172554cfe
Vigith Maurice <vigith@gmail.com>
2015-05-18 23:54:47 +0000

    changing the generic store interface args

--- src/rangestore/generic.go
+++ src/rangestore/generic.go
@@ -3,11 +3,11 @@ package rangestore
 // generic interface for any Store
 type Store interface {
 	// lookup cluster
-	ClusterLookup(string) ([]string, error)     // cluster
-	KeyLookup(string, string) ([]string, error) // cluster and key
+	ClusterLookup(string) (*[]string, error)     // cluster
+	KeyLookup(string, string) (*[]string, error) // cluster and key
 
 	// lookup reverse
-	KeyReverseLookup(string) ([]string, error)                     // just a reverse lookup on a node
-	KeyReverseLookupAttr(string, string) ([]string, error)         // reverse lookup where value and key are passed
-	KeyReverseLookupHint(string, string, string) ([]string, error) // reverse lookup where value and key are passed with an hint
+	KeyReverseLookup(string) (*[]string, error)                     // just a reverse lookup on a node
+	KeyReverseLookupAttr(string, string) (*[]string, error)         // reverse lookup where value and key are passed
+	KeyReverseLookupHint(string, string, string) (*[]string, error) // reverse lookup where value and key are passed with an hint
 }
