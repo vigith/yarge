dd98465bef0d687e48b2fa048e47bd86d3941def
Vigith Maurice <vigith@gmail.com>
2015-05-19 23:31:41 +0000

    adding ArrayToSet to set ops

--- src/rangeops/set.go
+++ src/rangeops/set.go
@@ -101,6 +101,29 @@ func Difference(set1 *[]string, set2 *[]string, r *[]string) {
 	return
 }
 
+// given an array, convert it into a set in place ie, remove duplicates from array
+// NOTE: Not the best algorithm, this is not space and time efficient.
+//       This function is written only for FileStore, because people make
+//       mistakes, not programs.
+func ArrayToSet(array *[]string) {
+	// map to store the
+	m := map[string]bool{}
+	// replace the current array in place such that, if we
+	// have not seen the element, insert it, But, insert such that
+	// insert at the position in the array where index = len(m) (HINT:
+	// m has unique elements of the array)
+	for _, v := range *array {
+		if _, seen := m[v]; !seen {
+			(*array)[len(m)] = v
+			m[v] = true
+		}
+	}
+	// re-slice s to the number of unique values
+	*array = (*array)[:len(m)]
+
+	return
+}
+
 ///////////////////////
 // Internal Fuctions //
 ///////////////////////
