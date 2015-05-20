d6a12a431f8fb09b946f84d41c757fd49eace0e9
Vigith Maurice <vigith@gmail.com>
2015-05-20 01:20:27 +0000

    mocking the teststore with test lookup

--- src/rangestore/filestore.go
+++ src/rangestore/filestore.go
@@ -10,34 +10,11 @@ package rangestore
 
 // // TODO: check whether the StorePath Exists, etc
 // func ConnectFStore(path string) (f FileStore, err error) {
-// 	f = FileStore{StorePath: path}
+// 	f = &FileStore{StorePath: path}
 // 	return f, nil
 // }
 
 // // function to connect to store
-// func (f FileStore) ClusterLookup(string) []string {
+// func (f *FileStore) ClusterLookup(string) []string {
 // 	return nil
 // }
-
-// given an array, convert it into a set in place ie, remove duplicates from array
-// NOTE: Not the best algorithm, this is not space and time efficient.
-//       This function is written only for FileStore, because people make
-//       mistakes, not programs.
-func ArrayToSet(array *[]string) {
-	// map to store the
-	m := map[string]bool{}
-	// replace the current array in place such that, if we
-	// have not seen the element, insert it, But, insert such that
-	// insert at the position in the array where index = len(m) (HINT:
-	// m has unique elements of the array)
-	for _, v := range *array {
-		if _, seen := m[v]; !seen {
-			(*array)[len(m)] = v
-			m[v] = true
-		}
-	}
-	// re-slice s to the number of unique values
-	*array = (*array)[:len(m)]
-
-	return
-}
--- src/rangestore/teststore.go
+++ src/rangestore/teststore.go
@@ -1,54 +1,281 @@
-// This file is only used for unit testing. We should be able to test
-// the rangeexpr operations without really connecting to a real store
+// This file implmenets a TestStore used only for unit testing. We should be able
+// to test the rangeexpr operations without really connecting to a real store
 
 package rangestore
 
 import (
 	"errors"
+	"fmt"
+	"rangeops"
 )
+
+// Test Range Structure
+/*
+{
+	"data": {
+		"prod": {
+			"vpc1": {
+				"log": {
+				"AUTHORS": [
+           "data@example.com"
+					],
+					"NODES": [
+						"data1001.data.example.com",
+						"data1002.data.example.com",
+            "data1003.data.example.com"
+                    ]
+				}
+			},
+			"vpc2": {
+				"log": {
+					"AUTHORS": [
+            "data@example.com"
+					],
+					"NODES": [
+						"data2001.data.example.com",
+						"data2002.data.example.com",
+            "data2003.data.example.com"
+                    ]
+				}
+			},
+			"vpc3": {
+				"log": {
+					"AUTHORS": [
+            "data@example.com"
+					],
+					"NODES": [
+						"data3001.data.example.com",
+						"data3002.data.example.com",
+            "data3003.data.example.com"
+                    ]
+				}
+			}
+		},
+		"qa": {
+			"vpc5": {
+				"log": {
+					"AUTHORS": [
+            "qa@example.com"
+					],
+					"NODES": [
+						"data5001.qa.example.com",
+            "data5002.qa.example.com"
+                    ]
+				}
+			}
+		}
+	},
+	"ops": {
+		"prod": {
+			"vpc1": {
+				"mon": {
+					"AUTHORS": [
+            "Ops"
+					],
+					"NODES": [
+            "mon1001.ops.example.com"
+                    ]
+				},
+				"range": {
+					"AUTHORS": [
+            "Vigith Maurice"
+					],
+					"NODES": [
+						"range1001.ops.example.com",
+						"range1002.ops.example.com",
+            "range1003.ops.example.com"
+                    ]
+				}
+			},
+			"vpc2": {
+				"mon": {
+					"AUTHORS": [
+            "Ops"
+					],
+					"NODES": [
+            "mon2001.ops.example.com"
+                    ]
+				}
+			}
+		}
+	}
+}
+*/
 
 type TestStore struct {
 	Test string // not for any good purpose
 }
 
 // function to connect to test store
-func ConnectTestStore(test string) (t TestStore, err error) {
-	t = TestStore{Test: test}
+func ConnectTestStore(test string) (t *TestStore, err error) {
+	t = &TestStore{Test: test}
 	return t, nil
 }
 
-// test clusterlookup
-func (t TestStore) ClusterLookup(cluster string) ([]string, error) {
-	if cluster == "error" {
-		return []string{}, errors.New("I am asked to return 'error'")
+// query map
+func queryMap(cluster string, key string) (*[]string, error) {
+	switch cluster {
+	case "RANGE":
+		return &[]string{"ops", "data"}, nil
+
+	case "ops":
+		return &[]string{"prod"}, nil
+	case "ops-prod":
+		return &[]string{"vpc1", "vpc2"}, nil
+	case "ops-prod-vpc1":
+		return &[]string{"range", "mon"}, nil
+	case "ops-prod-vpc2":
+		return &[]string{"mon"}, nil
+	case "ops-prod-vpc1-range":
+		if key == "NODES" {
+			return &[]string{"range1001.ops.example.com", "range1002.ops.example.com", "range1003.ops.example.com"}, nil
+		} else if key == "AUTHORS" {
+			return &[]string{"Vigith Maurice"}, nil
+		} else {
+			return &[]string{}, nil
+		}
+	case "ops-prod-vpc2-range":
+		if key == "NODES" {
+			return &[]string{}, errors.New("Cluster Not Found (ops-prod-vpc2-range)")
+		} else if key == "AUTHORS" {
+			return &[]string{}, errors.New("Cluster Not Found (ops-prod-vpc2-range)")
+		} else {
+			return &[]string{}, nil
+		}
+	case "ops-prod-vpc1-mon":
+		if key == "NODES" {
+			return &[]string{"mon1001.ops.example.com"}, nil
+		} else if key == "AUTHORS" {
+			return &[]string{"Ops"}, nil
+		} else {
+			return &[]string{}, nil
+		}
+	case "ops-prod-vpc2-mon":
+		if key == "NODES" {
+			return &[]string{"mon2001.ops.example.com"}, nil
+		} else if key == "AUTHORS" {
+			return &[]string{"Ops"}, nil
+		} else {
+			return &[]string{}, nil
+		}
+
+	case "data":
+		return &[]string{"prod", "qa"}, nil
+	case "data-prod":
+		return &[]string{"vpc1", "vpc2", "vpc3"}, nil
+	case "data-prod-vpc1":
+		return &[]string{"log"}, nil
+	case "data-prod-vpc2":
+		return &[]string{"log"}, nil
+	case "data-prod-vpc3":
+		return &[]string{"log"}, nil
+	case "data-prod-vpc1-log":
+		if key == "NODES" {
+			return &[]string{"data1001.data.example.com", "data1002.data.example.com", "data1003.data.example.com"}, nil
+		} else if key == "AUTHORS" {
+			return &[]string{"data@example.com"}, nil
+		} else {
+			return &[]string{}, nil
+		}
+	case "data-prod-vpc2-log":
+		if key == "NODES" {
+			return &[]string{"data2001.data.example.com", "data2002.data.example.com", "data2003.data.example.com"}, nil
+		} else if key == "AUTHORS" {
+			return &[]string{"data@example.com"}, nil
+		} else {
+			return &[]string{}, nil
+		}
+		return &[]string{"log"}, nil
+	case "data-prod-vpc3-log":
+		if key == "NODES" {
+			return &[]string{"data3001.data.example.com", "data3002.data.example.com", "data3003.data.example.com"}, nil
+		} else if key == "AUTHORS" {
+			return &[]string{"data@example.com"}, nil
+		} else {
+			return &[]string{}, nil
+		}
+
+	case "data-qa":
+		return &[]string{"vpc5"}, nil
+	case "data-qa-vpc5":
+		return &[]string{"log"}, nil
+	case "data-qa-vpc5-log":
+		if key == "NODES" {
+			return &[]string{"data5001.qa.example.com", "data5002.qa.example.com"}, nil
+		} else if key == "AUTHORS" {
+			return &[]string{"qa@example.com"}, nil
+		} else {
+			return &[]string{}, nil
+		}
 	}
 
-	return []string{"node1", "node2", "node3"}, nil
+	return nil, nil
+}
+
+// test clusterlookup
+func (t *TestStore) ClusterLookup(cluster *[]string) (*[]string, error) {
+
+	if (*cluster)[0] == "error" {
+		return &[]string{}, errors.New("I am asked to return 'error'")
+	}
+
+	var results = make([]string, 0)
+	for _, elem := range *cluster {
+		result, err := queryMap(elem, "")
+		if err != nil {
+			return &[]string{}, errors.New("Mock Map Failed!!")
+		}
+		results = append(results, *result...)
+	}
+
+	rangeops.ArrayToSet(&results)
+	return &results, nil
 }
 
 // test KeyLookup
-func (t TestStore) KeyLookup(cluster string, key string) ([]string, error) {
-	if cluster == "error" {
-		return []string{}, errors.New("I am asked to return 'error'")
+func (t *TestStore) KeyLookup(cluster *[]string, key string) (*[]string, error) {
+	if (*cluster)[0] == "error" {
+		return &[]string{}, errors.New("I am asked to return 'error'")
 	}
 
-	return []string{"foo"}, nil
+	var results = make([]string, 0)
+	for _, elem := range *cluster {
+		result, err := queryMap(elem, key)
+		if err != nil {
+			return &[]string{}, errors.New("Mock Map Failed!!")
+		}
+		fmt.Println(result)
+		results = append(results, *result...)
+	}
+
+	rangeops.ArrayToSet(&results)
+
+	return &results, nil
 }
 
 // test KeyReverseLookup
-func (t TestStore) KeyReverseLookup(cluster string, key string) ([]string, error) {
-	if cluster == "error" {
-		return []string{}, errors.New("I am asked to return 'error'")
+func (t *TestStore) KeyReverseLookup(key string) (*[]string, error) {
+	if key == "error" {
+		return &[]string{}, errors.New("I am asked to return 'error'")
 	}
 
-	return []string{"node1"}, nil
+	return &[]string{"node1"}, nil
 }
 
 // test KeyReverseLookupAttr
-func (t TestStore) KeyReverseLookupAttr(cluster string, key string, attr string) ([]string, error) {
-	if cluster == "error" {
-		return []string{}, errors.New("I am asked to return 'error'")
+func (t *TestStore) KeyReverseLookupAttr(key string, attr string) (*[]string, error) {
+	if key == "error" {
+		return &[]string{}, errors.New("I am asked to return 'error'")
 	}
 
-	return []string{"node2"}, nil
+	return &[]string{"node2"}, nil
+}
+
+// test KeyReverseLookupHint
+func (t *TestStore) KeyReverseLookupHint(key string, attr string, hint string) (*[]string, error) {
+	if key == "error" {
+		return &[]string{}, errors.New("I am asked to return 'error'")
+	}
+
+	return &[]string{"node3"}, nil
 }
