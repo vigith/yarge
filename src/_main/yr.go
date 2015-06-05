e64f125749580a1bc10a04b8bbc7c32edf227f94
Anuj Maurice <amaurice@amaurice-mn1.linkedin.biz>
2015-06-05 15:34:57 +0530

    adding command line options

--- src/_main/yr.go
+++ src/_main/yr.go
@@ -3,23 +3,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		name := os.Args[0]
-		fmt.Printf("Usage: %v \"EXPRESSION\"\n", name)
-		fmt.Printf("Example: %v \"%%RANGE\n", name)
-		os.Exit(1)
+//globals
+var debug bool
+var help bool
+var timing bool
+var vip string
+var query string
+
+func init() {
+	parseFlags()
+	if help == true {
+		printHelp()
+		os.Exit(0)
 	}
-	query := os.Args[1]
-	res, err := http.Get(fmt.Sprintf("http://localhost:9999/v1/range/list?%s", url.QueryEscape(query)))
+	return
+}
+
+func parseFlags() {
+	flag.BoolVar(&debug, "debug", false, "enable debug")
+	flag.BoolVar(&help, "help", false, "enable help")
+	flag.BoolVar(&timing, "timing", false, "enable timing")
+	flag.StringVar(&vip, "vip", "localhost", "vip endpoint")
+	flag.StringVar(&query, "query", "", "query")
+
+	flag.Parse()
+	return
+}
+
+func printHelp() {
+	fmt.Println(
+		`Usage: rangerclient [OPTIONS]
+	--debug......................Run client in debug mode
+	--help.......................Prints this documentation
+	--timing.....................profiling of execution time
+	--query......................query string
+	--vip........................Range vip endpoint`)
+	return
+}
+
+func main() {
+	t1 := time.Now()
+	if query == "" {
+		fmt.Println("Need a query to execute")
+		os.Exit(0)
+	}
+	if vip == "localhost" {
+		vip = "localhost:9999"
+	}
+	res, err := http.Get(fmt.Sprintf("http://%s/v1/range/list?%s", vip, url.QueryEscape(query)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,4 +74,10 @@ func main() {
 	} else {
 		fmt.Printf("%s\n", results)
 	}
+
+	t2 := time.Now()
+	if timing == true {
+		fmt.Printf("Took %v to run", t2.Sub(t1))
+	}
+
 }
