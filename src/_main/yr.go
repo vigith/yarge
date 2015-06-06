dfe97e053175c4c8489fa8236e9456c04cf4cdbb
Anuj Maurice <anuj.maurice@gmail.com>
2015-06-06 23:07:07 +0530

    fixing minor glitch

--- src/_main/yr.go
+++ src/_main/yr.go
@@ -10,7 +10,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	//"time"
 )
 
 //globals
@@ -18,8 +17,6 @@ var debug bool
 var help bool
 var timing bool
 var vip string
-
-//var query string
 
 func init() {
 	parseFlags()
@@ -35,7 +32,6 @@ func parseFlags() {
 	flag.BoolVar(&help, "help", false, "enable help")
 	flag.BoolVar(&timing, "timing", false, "enable timing")
 	flag.StringVar(&vip, "vip", "localhost", "vip endpoint")
-	//flag.StringVar(&query, "query", "", "query")
 
 	flag.Parse()
 	return
@@ -44,25 +40,21 @@ func parseFlags() {
 func printHelp() {
 	fmt.Println(
 		`	Usage: rangerclient [OPTIONS] <query>
-	eg: go run yr.go --timing --vip 192.168.1.33:9999 %RANGE
-	--debug......................Run client in debug mode
-	--help.......................Prints this documentation
-	--timing.....................profiling of execution time
-	--vip........................Range vip endpoint`)
+	eg: yr %RANGE
+	--debug .................... Run client in debug mode
+	--help ..................... Prints this documentation
+	--timing ................... profiling of execution time
+	--vip ...................... Range vip endpoint`)
+	os.Exit(0)
 	return
 }
 
 func main() {
 	query := ""
-	if len(os.Args) < 2 {
-		name := os.Args[0]
-		fmt.Printf("Usage: %v \"EXPRESSION\"\n", name)
-		fmt.Printf("Example: %v \"%%RANGE\n", name)
-		os.Exit(1)
+	if len(os.Args) == 1 {
+		printHelp()
 	}
-	if len(os.Args) > 1 {
-		query = os.Args[len(os.Args)-1] //trying to get the last element of an array
-	}
+	query = os.Args[len(os.Args)-1] //trying to get the last element of an array
 	if vip == "localhost" {
 		vip = "localhost:9999"
 	}
@@ -81,6 +73,6 @@ func main() {
 		fmt.Printf("%s\n", results)
 	}
 	if timing == true {
-		fmt.Printf("took %sms \n", res.Header.Get("Range-Expand-Microsecond"))
+		fmt.Printf("Range-Expand-Microsecond : %sms \n", res.Header.Get("Range-Expand-Microsecond"))
 	}
 }
