eb1f0a2b0a294bf55da0f0c5134d62b411bbbca7
Anuj Maurice <anuj.maurice@gmail.com>
2015-06-06 19:18:49 +0530

    included range expand time

--- src/_main/yr.go
+++ src/_main/yr.go
@@ -10,7 +10,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"time"
+	//"time"
 )
 
 //globals
@@ -18,7 +18,8 @@ var debug bool
 var help bool
 var timing bool
 var vip string
-var query string
+
+//var query string
 
 func init() {
 	parseFlags()
@@ -34,7 +35,7 @@ func parseFlags() {
 	flag.BoolVar(&help, "help", false, "enable help")
 	flag.BoolVar(&timing, "timing", false, "enable timing")
 	flag.StringVar(&vip, "vip", "localhost", "vip endpoint")
-	flag.StringVar(&query, "query", "", "query")
+	//flag.StringVar(&query, "query", "", "query")
 
 	flag.Parse()
 	return
@@ -42,20 +43,25 @@ func parseFlags() {
 
 func printHelp() {
 	fmt.Println(
-		`Usage: rangerclient [OPTIONS]
+		`	Usage: rangerclient [OPTIONS] <query>
+	eg: go run yr.go --timing --vip 192.168.1.33:9999 %RANGE
 	--debug......................Run client in debug mode
 	--help.......................Prints this documentation
 	--timing.....................profiling of execution time
-	--query......................query string
 	--vip........................Range vip endpoint`)
 	return
 }
 
 func main() {
-	t1 := time.Now()
-	if query == "" {
-		fmt.Println("Need a query to execute")
-		os.Exit(0)
+	query := ""
+	if len(os.Args) < 2 {
+		name := os.Args[0]
+		fmt.Printf("Usage: %v \"EXPRESSION\"\n", name)
+		fmt.Printf("Example: %v \"%%RANGE\n", name)
+		os.Exit(1)
+	}
+	if len(os.Args) > 1 {
+		query = os.Args[len(os.Args)-1] //trying to get the last element of an array
 	}
 	if vip == "localhost" {
 		vip = "localhost:9999"
@@ -74,10 +80,7 @@ func main() {
 	} else {
 		fmt.Printf("%s\n", results)
 	}
-
-	t2 := time.Now()
 	if timing == true {
-		fmt.Printf("Took %v to run", t2.Sub(t1))
+		fmt.Printf("took %sms \n", res.Header.Get("Range-Expand-Microsecond"))
 	}
-
 }
